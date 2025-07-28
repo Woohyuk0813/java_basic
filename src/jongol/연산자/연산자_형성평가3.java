7328649df52d5f018dbda557b68388d5f912e3e5
Kang Woo Hyuk <wh081312@gmail.com>
2025-07-28 16:40:43 +0900

    20250728

--- src/jongol/연산자/연산자_형성평가3.java
+++ src/jongol/연산자/연산자_형성평가3.java
@@ -7,22 +7,17 @@ public class 연산자_형성평가3 {
 
         Scanner scanner = new Scanner(System.in);
 
-        // 사용자 입력 받기
-        System.out.print("가로 길이를 입력하세요: ");
         int width = scanner.nextInt();
-
-        System.out.print("세로 길이를 입력하세요: ");
         int height = scanner.nextInt();
 
-        // 가공
-        width = width + 5;       // 가로 5 증가
-        height = height * 2;     // 세로 2배
+        width = width + 5;
+        height = height * 2;
 
-        int area = width * height;  // 넓이 계산
+        int area = width * height;
 
-        // 결과 출력
-        System.out.println("가로 길이: " + width);
-        System.out.println("세로 길이: " + height);
-        System.out.println("넓이: " + area);
+
+        System.out.println("width = " + width);
+        System.out.println("length = " + height);
+        System.out.println("area = " + area);
     }
 }
--- src/jongol/연산자/연산자_형성평가4.java
+++ src/jongol/연산자/연산자_형성평가4.java
@@ -1,4 +1,19 @@
 package jongol.연산자;
 
+import java.util.Scanner;
+
 public class 연산자_형성평가4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.printf("%d ",++a);
+        System.out.printf("%d\n",b--);
+
+        System.out.printf("%d ",a);
+        System.out.printf("%d",b);
+    }
 }
