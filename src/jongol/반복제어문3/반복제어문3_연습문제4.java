56ebd32fbe0b9a4e7a2e7937f8dc3250d8e67b4a
Kang Woo Hyuk <wh081312@gmail.com>
2025-07-29 19:46:37 +0900

    123

---
+++ src/jongol/반복제어문3/반복제어문3_연습문제4.java
@@ -0,0 +1,20 @@
+package jongol.반복제어문3;
+
+import java.util.Scanner;
+
+public class 반복제어문3_연습문제4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = n ; j >= 1; j--){
+                if(i<j){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
