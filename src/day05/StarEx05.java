5555130a4567334383007d540eeb71bfc938c96e
Kang Woo Hyuk <wh081312@gmail.com>
2025-07-29 11:15:52 +0900

    123

---
+++ src/day05/StarEx05.java
@@ -0,0 +1,17 @@
+package day05;
+
+public class StarEx05 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if(i<j){
+                    System.out.printf("*");
+                }else{
+                    System.out.printf("_");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+//오른쪽 역 삼각형 별 찍기
