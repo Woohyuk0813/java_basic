7a23e18783b3a6160e6063a0673384eb020f7709
Kang Woo Hyuk <wh081312@gmail.com>
2025-08-01 14:41:15 +0900

    123

---
+++ src/jongol/문자열1/문자열1_연습문제5.java
@@ -0,0 +1,19 @@
+package jongol.문자열1;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class 문자열1_연습문제5 {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String input = "우리나라 대한민국!";
+        System.out.println(input);
+        byte[] utf8Bytes = input.getBytes(StandardCharsets.UTF_8);
+        System.out.printf("위 문자열의 길이는 %d입니다.",utf8Bytes.length);
+
+
+    }
+}
+
+
