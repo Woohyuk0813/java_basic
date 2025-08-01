7bb04f3f4f3c48df0d0dfec38f48e1442574413e
Kang Woo Hyuk <wh081312@gmail.com>
2025-08-01 18:11:01 +0900

    123

--- src/gpt_배열/문제2.java
+++ src/gpt_배열/문제2.java
@@ -12,8 +12,10 @@ public class 문제2 {
         int max = array[0];
 
         for (int i = 0; i < array.length; i++) {
-
+            if(array[i]>max){
+                max = array[i];
+            }
         }
-
+        System.out.println(max);
     }
 }
