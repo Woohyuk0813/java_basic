add3bcfb922545c550ea85e1cd6654b24de414fa
Kang Woo Hyuk <wh081312@gmail.com>
2025-08-05 14:37:25 +0900

    123

---
+++ src/javabasic_02/day10/ShopService.java
@@ -0,0 +1,34 @@
+package javabasic_02.day10;
+
+public class ShopService {
+    // 싱글톤 클래스 객체를 담을 인스턴스 변수
+    private static ShopService instance;
+
+    // 생성자를 private 선언 (외부 new 못하게)
+    private ShopService() {
+    }
+
+    public static ShopService getInstance() {
+        if (instance == null) {
+            instance = new ShopService();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        ShopService s1 = ShopService.getInstance();
+        ShopService s2 = ShopService.getInstance();
+        ShopService s3 = ShopService.getInstance();
+
+        System.out.println(s1.toString());
+        System.out.println(s2.toString());
+        System.out.println(s3.toString());
+
+        if (s1 == s2)
+            System.out.println("같은 ShopService 객체입니다.");
+        if (s2 == s3)
+            System.out.println("같은 ShopService 객체입니다.");
+
+
+    }
+}
