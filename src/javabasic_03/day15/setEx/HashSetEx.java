4c6e76947948c3e9351059d3a4ec92442edffa4b
Kang Woo Hyuk <wh081312@gmail.com>
2025-08-14 10:32:15 +0900

    123

---
+++ src/javabasic_03/day15/setEx/HashSetEx.java
@@ -0,0 +1,21 @@
+package javabasic_03.day15.setEx;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<String>();
+
+
+        set.add("Java");
+        set.add("JDBC");
+        set.add("JSP");
+        set.add("JSP");
+        set.add("Spring");
+
+        int size = set.size();
+        System.out.println(size);
+    }
+
+}
---
+++ src/javabasic_03/day15/setEx/Member.java
@@ -0,0 +1,5 @@
+package javabasic_03.day15.setEx;
+
+public class Member {
+
+}
