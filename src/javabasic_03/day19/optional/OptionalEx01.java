22ce05578fc06bdca73b3de21761ea6fe2bd2808
Woohyuk0813 <wh081312@gmail.com>
2025-08-22 16:31:53 +0900

    123

--- src/javabasic_03/day19/optional/OptionalEx01.java
+++ src/javabasic_03/day19/optional/OptionalEx01.java
@@ -1,12 +1,13 @@
 package javabasic_03.day19.optional;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalDouble;
 
 public class OptionalEx01 {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         double avg = list.stream().mapToInt(Integer::intValue).average().getAsDouble();
 
         //방법1
