00c666ddbdb01fe868d11f91914c90d696bd5c46
Woohyuk0813 <wh081312@gmail.com>
2025-09-08 18:22:36 +0900

    123

--- src/loop_array/JavaBasic2.java
+++ src/loop_array/JavaBasic2.java
@@ -3,9 +3,9 @@ package loop_array;
 public class JavaBasic2 {
     public static void main(String[] args) {
         int sum = 0;
-        for(int i = 1; i<=100; i++){
-            if(i % 3 == 0){
-                sum = i +sum;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                sum = i + sum;
             }
 
         }
@@ -13,3 +13,4 @@ public class JavaBasic2 {
 
     }
 }
+
