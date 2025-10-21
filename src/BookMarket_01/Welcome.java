be9985da6b09f6efc98623634c33546997318332
Woohyuk0813 <wh081312@gmail.com>
2025-10-21 17:50:35 +0900

    1232

--- src/BookMarket_01/Welcome.java
+++ src/BookMarket_01/Welcome.java
@@ -153,7 +153,6 @@ public class Welcome {
                     break;
                 }
             }
-
             if (flag) {
                 System.out.println("장바구니에 추가하겠습니까? Y | N ");
                 str = input.nextLine();
